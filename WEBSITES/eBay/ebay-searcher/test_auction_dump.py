9ae13699ab3a818202e6d2b46016a756836cbf57
Tapziii <108729923+Tapziii@users.noreply.github.com>
2026-08-08 22:00:21 +0300

    Delete WEBSITES/eBay directory

--- WEBSITES/eBay/ebay-searcher/test_auction_dump.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdout.reconfigure(encoding="utf-8")
-from ebay_client import EBayClient
-client = EBayClient()
-url = client._build_url("www.ebay.com", "iphone", None, None, 10, buy_it_now=False, page=1) + "&LH_Auction=1"
-resp = client.session.get(url, headers={"Referer": "https://www.ebay.com/"})
-with open("dump_auction.html", "w", encoding="utf-8") as f:
-    f.write(resp.text)
-print(len(resp.text))
--- WEBSITES/eBay/ebay-searcher/test_count2.py
+++
@@ -1,8 +0,0 @@
-from ebay_client import EBayClient
-from bs4 import BeautifulSoup
-client = EBayClient()
-client._warm_session("www.ebay.com")
-url = client._build_url("www.ebay.com", "canon powershot", None, 100, 100)
-resp = client.session.get(url)
-soup = BeautifulSoup(resp.text, "html.parser")
-print(soup.prettify()[:1500])
